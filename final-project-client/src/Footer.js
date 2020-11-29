cf20051d0baf06450d02431d7fca97de8b20fb97
Janki Vipulkumar Jariwala <jjariwal@isimtl.local>
2020-11-29 15:14:07 -0500

    Final project done

--- final-project-client/src/Footer.js
+++ final-project-client/src/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 class Footer extends React.Component{
     constructor(props) {
         super(props);
         // contructor code here
     }
-
-    render(){
-        return (
-            <footer style={{color:"red"}}>
-                &copy;{this.props.authorName}
+         render(){
+            return (
+            <footer className={styles.Footer}>
+                 &copy;{this.props.authorName}
             </footer>)
         }
     }
